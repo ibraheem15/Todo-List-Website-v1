28b035d3ec007c5bc7fb30ac71dc947ce20c59a6
ibraheem15 <103947216+ibraheem15@users.noreply.github.com>
2022-09-10 22:52:55 +0500

    Adding Date to Title of Todo List

--- date.js
+++ date.js
@@ -1,24 +1,31 @@
 module.exports.getDate = function getDate() {
-    var today = new Date();
-    var options = {
-      weekday: "long",
-      day: "numeric",
-      month: "long",
-    };
-    var day = today.toLocaleDateString("en-US", options);
-  
-    return day;
-  }
-// module.exports = getDate(); //we can do this stuff as well but we will use function cal in index.js
-//because in this way it will call the function automatically and we want to call it when we want to
-function getDay() {
   var today = new Date();
   var options = {
     weekday: "long",
+    day: "numeric",
+    month: "long",
   };
   var day = today.toLocaleDateString("en-US", options);
 
   return day;
+};
+// module.exports = getDate(); //we can do this stuff as well but we will use function cal in index.js
+//because in this way it will call the function automatically and we want to call it when we want to
+function getDay() {
+  var today = new Date();
+  var dd = String(today.getDate()).padStart(2, "0");
+  var mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
+  var yyyy = today.getFullYear();
+
+  today = mm + "/" + dd + "/" + yyyy;
+
+  /* var today = new Date();
+  var options = {
+    weekday: "long",
+  };
+  var day = today.toLocaleDateString("en-US", options);
+ */
+  return today;
 }
 
 module.exports.getDay = getDay;
